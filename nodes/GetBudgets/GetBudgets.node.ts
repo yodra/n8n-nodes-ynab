59b75b4bf610f080557c0cd3bb78020246239148
yodralopez <yodralopez@gmail.com>
2025-06-09 15:09:31 +0100

    modify node to get the APIKey from credentials

--- nodes/GetBudgets/GetBudgets.node.ts
+++ nodes/GetBudgets/GetBudgets.node.ts
@@ -12,88 +12,79 @@ import { YNABBudgetResponse } from '../YNABBudgetResponse';
 
 export class GetBudgets implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'Get Budgets',
+		displayName: 'YNAB - Get Budgets',
 		name: 'getBudgets',
 		group: ['transform'],
 		version: 1,
-		description: 'Get Budgets',
+		description: 'YNAB Get Budgets',
 		defaults: {
 			name: 'Get Budgets',
 		},
 		inputs: ['main'] as NodeConnectionType[],
 		outputs: ['main'] as NodeConnectionType[],
-		properties: [
+		credentials: [
 			{
-				displayName: 'API Key',
-				name: 'apiKey',
-				type: 'string',
-				typeOptions: { password: true },
-				default: '',
+				name: 'ynabApi',
 				required: true,
-				description: 'YNAB API Key',
 			},
 		],
+		properties: [],
 	};
 
 	async execute(
 		this: IExecuteFunctions,
 	): Promise<INodeExecutionData[][] | NodeExecutionWithMetadata[][] | null> {
-		const items = this.getInputData();
 		const returnData: INodeExecutionData[] = [];
+		const apiKey = await this.getCredentials('ynabApi');
 
-		for (let i = 0; i < items.length; i++) {
-			// const apiKey = await this.getCredentials('ynabApi');
-			const apiKey = this.getNodeParameter('apiKey', i) as string;
+		try {
+			const response = await new Promise<YNABBudgetResponse>((resolve, reject) => {
+				const options = {
+					hostname: 'api.ynab.com',
+					path: '/v1/budgets?include_accounts=false',
+					method: 'GET',
+					headers: {
+						Authorization: `${apiKey.token ? `Bearer ${apiKey.token}` : ''}`,
+						'Content-Type': 'application/json',
+					},
+				};
 
-			try {
-				const response = await new Promise<YNABBudgetResponse>((resolve, reject) => {
-					const options = {
-						hostname: 'api.ynab.com',
-						path: '/v1/budgets?include_accounts=false',
-						method: 'GET',
-						headers: {
-							Authorization: `Bearer ${apiKey}`,
-							'Content-Type': 'application/json',
-						},
-					};
+				const req = https.request(options, (res) => {
+					let data = '';
 
-					const req = https.request(options, (res) => {
-						let data = '';
-
-						res.on('data', (chunk) => {
-							data += chunk;
-						});
-
-						res.on('end', () => {
-							if (res.statusCode && res.statusCode >= 200 && res.statusCode < 300) {
-								resolve(JSON.parse(data));
-							} else {
-								const errorData = JSON.parse(data) as YNABError;
-								reject(
-									new Error(
-										`Error getting budgets: ${errorData.error?.detail || res.statusMessage}`,
-									),
-								);
-							}
-						});
+					res.on('data', (chunk) => {
+						data += chunk;
 					});
 
-					req.on('error', (error) => {
-						reject(error);
+					res.on('end', () => {
+						if (res.statusCode && res.statusCode >= 200 && res.statusCode < 300) {
+							resolve(JSON.parse(data));
+						} else {
+							const errorData = JSON.parse(data) as YNABError;
+							reject(
+								new Error(
+									`Error getting budgets: ${errorData.error?.detail || res.statusMessage} / ${apiKey.toString()}`,
+								),
+							);
+						}
 					});
-
-					req.end();
 				});
 
-				returnData.push({
-					json: response,
+				req.on('error', (error) => {
+					reject(error);
 				});
-			} catch (error) {
-				if (error instanceof Error) {
-					throw Error(`Error getting budgets: ${error.message}`);
-				}
-				throw error;
+
+				req.end();
+			});
+
+			returnData.push({
+				json: response,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				throw Error(`Error getting budgets: ${error.message}`);
 			}
+			throw error;
 		}
 
 		return [returnData];
