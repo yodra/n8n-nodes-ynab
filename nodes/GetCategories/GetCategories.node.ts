9a7d5b6c93df3013194f630f378057580df589d5
yodralopez <yodralopez@gmail.com>
2025-08-05 18:11:23 +0100

    add categories node

---
+++ nodes/GetCategories/GetCategories.node.ts
@@ -0,0 +1,105 @@
+import {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeConnectionType,
+	NodeExecutionWithMetadata,
+} from 'n8n-workflow';
+import https from 'https';
+import { YNABError } from '../YNABError';
+import { YNABBudgetResponse } from '../YNABBudgetResponse';
+
+export class GetCategories implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'YNAB - Get Categories',
+		name: 'getCategories',
+		group: ['transform'],
+		version: 1,
+		description: 'YNAB Get Categories',
+		defaults: {
+			name: 'Get Categories',
+		},
+		inputs: ['main'] as NodeConnectionType[],
+		outputs: ['main'] as NodeConnectionType[],
+		credentials: [
+			{
+				name: 'ynabApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Budget ID',
+				name: 'budget_id',
+				type: 'string',
+				default: '',
+				placeholder: 'Placeholder value',
+				description: 'Budget identifier. Check your budgets to find the correct value.',
+			},
+		],
+	};
+
+	async execute(
+		this: IExecuteFunctions,
+	): Promise<INodeExecutionData[][] | NodeExecutionWithMetadata[][] | null> {
+		const returnData: INodeExecutionData[] = [];
+		const apiKey = await this.getCredentials('ynabApi');
+		const items = this.getInputData();
+
+		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+			try {
+				const budgetId = this.getNodeParameter('budget_id', itemIndex, '') as string;
+				const response = await new Promise<YNABBudgetResponse>((resolve, reject) => {
+					const options = {
+						hostname: 'api.ynab.com',
+						path: `/v1/budgets/${budgetId}/categories`,
+						method: 'GET',
+						headers: {
+							Authorization: `${apiKey.token ? `Bearer ${apiKey.token}` : ''}`,
+							'Content-Type': 'application/json',
+						},
+					};
+
+					const req = https.request(options, (res) => {
+						let data = '';
+
+						res.on('data', (chunk) => {
+							data += chunk;
+						});
+
+						res.on('end', () => {
+							if (res.statusCode && res.statusCode >= 200 && res.statusCode < 300) {
+								resolve(JSON.parse(data));
+							} else {
+								const errorData = JSON.parse(data) as YNABError;
+								reject(
+									new Error(
+										`Error getting budgets: ${errorData.error?.detail || res.statusMessage} / ${apiKey.toString()}`,
+									),
+								);
+							}
+						});
+					});
+
+					req.on('error', (error) => {
+						reject(error);
+					});
+
+					req.end();
+				});
+
+				returnData.push({
+					json: response,
+				});
+			} catch (error) {
+				if (error instanceof Error) {
+					throw Error(`Error getting budgets: ${error.message}`);
+				}
+				throw error;
+			}
+		}
+
+		return [returnData];
+	}
+}
